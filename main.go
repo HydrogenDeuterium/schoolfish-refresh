e3b8bcd00d3783f9867618f91a04e5b9cca388f7
氘化氢 <36532302+HydrogenDeuterium@users.noreply.github.com>
2021-08-25 00:11:53 +0800

    通过测试，删除测试； 写一堆 db 的 init 太丑了，打包成一个在main里统一处理吧。

--- main.go
+++ main.go
@@ -6,18 +6,17 @@ import (
 )
 
 func main() {
-	rdb := models.RedisInit()
-	mdb := models.MysqlInit()
+	DB := models.DBInit()
+	defer DB.Close()
 
-	models.RedisTest()
-
-	defer rdb.Close()
-	defer mdb.Close()
 	r := gin.Default()
+	defer func(r *gin.Engine) {
+		_ = r.Run()
+	}(r) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
-	_ = r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
--- models/init.go
+++ models/init.go
@@ -1,14 +1,12 @@
 package models
 
 import (
-	"fmt"
 	"github.com/go-redis/redis"
 	"github.com/jinzhu/gorm"
 	"os"
-	"time"
 )
 
-func RedisInit() *redis.Client {
+func redisInit() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("redis_address"),  // use default Addr
 		Password: os.Getenv("redis_password"), // no password set
@@ -17,46 +15,25 @@ func RedisInit() *redis.Client {
 	return rdb
 }
 
-func MysqlInit() *gorm.DB {
+func mysqlInit() *gorm.DB {
 	db, _ := gorm.Open("mysql",
 		os.Getenv("mysql_jdbc"))
 	return db
 }
 
-func RedisTest() {
-	client := RedisInit()
+type DBs struct {
+	redis *redis.Client
+	mysql *gorm.DB
+}
 
-	//过期时间：1 秒
-	err := client.Set("key1", "random-value", 1*time.Second).Err()
-	if err != nil {
-		panic(err)
+func (dbs DBs) Close() {
+	_ = dbs.redis.Close()
+	_ = dbs.mysql.Close()
+}
+
+func DBInit() DBs {
+	return DBs{
+		redisInit(),
+		mysqlInit(),
 	}
-
-	//1 秒之内，可以获取到
-	val, err := client.Get("key1").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key1=", val)
-
-	//睡眠两秒，过期了 获取不到了。
-	time.Sleep(2 * time.Second)
-	val, err = client.Get("key1").Result()
-	if err == redis.Nil {
-		fmt.Println("key1 does not exist now")
-	} else if err != nil {
-		panic(err)
-	}
-	fmt.Println("key1", val)
-
-	val2, err := client.Get("key2").Result()
-	if err == redis.Nil {
-		fmt.Println("key2 does not exist even")
-	} else if err != nil {
-		panic(err)
-	}
-	fmt.Println("key2=", val2)
-
-	// Output: key value
-	// key2 does not exist
 }
