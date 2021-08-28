f0edb4d77480225bd10b9d122c20be7d5021fdad
氘化氢 <36532302+HydrogenDeuterium@users.noreply.github.com>
2021-08-28 08:09:56 +0800

    提取测试

--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 import os
+from typing import Union
 
 import httpx
 
@@ -15,20 +16,41 @@ data = 'data'
 msg = 'msg'
 
 
+def verify_200(r: httpx.Response) -> Union[dict, list]:
+    assert r.status_code == 200
+    assert isinstance(j := r.json(), dict)
+    print(j)
+    assert j[code] == 200
+    assert j[msg] == ""
+    return j[data]
+
+
+def verify_400(r: httpx.Response) -> str:
+    assert r.status_code == 200
+    assert isinstance(j := r.json(), dict)
+    print(j)
+    assert j[code] == 400
+    assert j[data] is None
+    return j[msg]
+
+
+def verify_500(r: httpx.Response) -> None:
+    assert r.status_code == 200
+    assert isinstance(j := r.json(), dict)
+    print(j)
+    assert j[code] == 500
+    assert j[msg] == ""
+
+
 def test_getcode():
     p = {"email": "123@456"}
     c.delete("/auth", params=p)
 
-    r = c.get("/auth", params=p)
-    assert r.status_code == 200
-    j = r.json()
-    assert j['code'] == 200
-    assert j['data'] is None
-    assert isinstance(j['msg'], str)
+    d = verify_200(c.get("/auth", params=p))
+    print("code=", d)
+    m = verify_400(c.get("/auth", params=p))
+    assert m == ""
 
-    r = c.get("/auth", params=p)
-    assert r.status_code == 200
-    j = r.json()
-    assert j['code'] == 400
-    assert j['data'] is None
-    assert isinstance(j['msg'], str)
+
+if __name__ == "__main__":
+    pass
