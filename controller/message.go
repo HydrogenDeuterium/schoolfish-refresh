04f301a9ca3c1676f9385fd78175859e5319076d
氘化氢 <36532302+HydrogenDeuterium@users.noreply.github.com>
2021-09-04 11:02:23 +0800

    发送消息

--- controller/message.go
+++ controller/message.go
@@ -5,6 +5,7 @@ import (
 	"schoolfish-refresh/middleware"
 	"schoolfish-refresh/model"
 	"schoolfish-refresh/util"
+	"strconv"
 )
 
 func Message(c *gin.RouterGroup, DB model.DBGroup) {
@@ -31,8 +32,22 @@ func Message(c *gin.RouterGroup, DB model.DBGroup) {
 
 	c.POST("/:uid", func(c *gin.Context) {
 		self, _ := c.Get("uid")
-		if self == "" {
+		self, err := strconv.Atoi(self.(string))
+		if err != nil {
 			util.ReturnError(c, "不正确的uid！")
+			return
+		}
+		other := c.Param("uid")
+		uid, _ := strconv.Atoi(other)
+		text := c.Param("text")
+		message := model.Messages{
+			From: self.(uint),
+			To:   uint(uid),
+			Text: &text,
+		}
+		err = DB.Mysql.Model(&message).Create(&message).Error
+		if err != nil {
+			util.ReturnInternal(c)
 			return
 		}
 		util.ReturnGood(c, nil)
